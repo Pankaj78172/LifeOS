cec3f27d811fe6e22cfd36c077b23e6a5181da9e
Pankaj78172 <pankajpaliwal61@gmail.com>
2026-04-24 20:49:44 -0600

    Backend setup

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+
+const app = express()
+
+
+app.get("/", (req, res)=>{
+    res.send("LifeOS Backend Running")
+})
+
+
+
+app.listen(5000, ()=>{
+    console.log("Server is running")
+})
+
+
+
+
+

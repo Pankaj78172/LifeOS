c25d106f3ceb941b0d49cd5053715c5be19ebbac
Pankaj78172 <pankajpaliwal61@gmail.com>
2026-04-27 23:10:05 -0600

    Add backend error logging

--- server/src/app.js
+++ server/src/app.js
@@ -1,23 +1,27 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express")
+const cors = require("cors")
 const authRoutes = require("./routes/authRoutes")
-
-const expnseRouter = require('./routes/expenseRoutes')
+const expnseRouter = require("./routes/expenseRoutes")
 const taskRoutes = require("./routes/taskRoutes")
 
 const app = express()
 
-
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://your-frontend-url.vercel.app"
-  ],
-  credentials: true
-}))
-
+app.use(cors())
 app.use(express.json())
 
+app.get("/", (req, res) => {
+  res.send("Server is running")
+})
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API test works" })
+})
+
+app.use("/api/expenses", expnseRouter)
+app.use("/api/tasks", taskRoutes)
+app.use("/api/auth", authRoutes)
+
+// keep this LAST
 app.use((err, req, res, next) => {
   console.error("GLOBAL ERROR:", err)
 
@@ -27,28 +31,4 @@ app.use((err, req, res, next) => {
   })
 })
 
-app.use("/api/expenses", expnseRouter)
-app.use("/api/tasks", taskRoutes)
-app.use("/api/auth", authRoutes)
-
-app.get("/", (req, res)=>{
-    res.send("Server is running")
-})
-
-app.get("/api/test", (req, res) => {
-  res.json({ message: "API test works" })
-})
-
-
-
-
 module.exports = app
-
-
-
-
-
-
-
-
-
